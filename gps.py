b7757e304236fcaaa3f66dffa8c6e26a887aa30a
spanicker <shubhie@gmail.com>
2015-12-22 09:19:31 -0800

    move student files to subdir

--- gps.py
+++ gps.py
@@ -13,97 +13,97 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 class MainPage(webapp2.RequestHandler):
     def get(self):
         vars = {'page': 'home'}
-    	template = JINJA_ENVIRONMENT.get_template('index.html')
+    	template = JINJA_ENVIRONMENT.get_template('student/index.html')
         self.response.out.write(template.render(vars))
 
 class StudentHomePage(webapp2.RequestHandler):
     def get(self):
         vars = {'page': 'home'}
-        template = JINJA_ENVIRONMENT.get_template('studenthome.html')
+        template = JINJA_ENVIRONMENT.get_template('student/studenthome.html')
         self.response.out.write(template.render(vars))
 
 class StudentLifePage(webapp2.RequestHandler):
     def get(self):
         vars = {'page': 'home'}
-    	template = JINJA_ENVIRONMENT.get_template('studentlife.html')
+    	template = JINJA_ENVIRONMENT.get_template('student/studentlife.html')
         self.response.out.write(template.render(vars))
 
 class UniversityChoicePage(webapp2.RequestHandler):
     def get(self):
         vars = {'page': 'home'}
-    	template = JINJA_ENVIRONMENT.get_template('universitychoice.html')
+    	template = JINJA_ENVIRONMENT.get_template('student/universitychoice.html')
         self.response.out.write(template.render(vars))
 
 class DevelopSkillsPage(webapp2.RequestHandler):
     def get(self):
         vars = {'page': 'home'}
-    	template = JINJA_ENVIRONMENT.get_template('developskills.html')
+    	template = JINJA_ENVIRONMENT.get_template('student/developskills.html')
         self.response.out.write(template.render(vars))
 
 class ApplyPage(webapp2.RequestHandler):
     def get(self):
         vars = {'page': 'home'}
-    	template = JINJA_ENVIRONMENT.get_template('apply.html')
+    	template = JINJA_ENVIRONMENT.get_template('student/apply.html')
         self.response.out.write(template.render(vars))
 
 class ApplicationFormPage(webapp2.RequestHandler):
     def get(self):
         vars = {'page': 'home'}
-    	template = JINJA_ENVIRONMENT.get_template('applicationform.html')
+    	template = JINJA_ENVIRONMENT.get_template('student/applicationform.html')
         self.response.out.write(template.render(vars))
 
 class CostsPage(webapp2.RequestHandler):
     def get(self):
         vars = {'page': 'home'}
-    	template = JINJA_ENVIRONMENT.get_template('costs.html')
+    	template = JINJA_ENVIRONMENT.get_template('student/costs.html')
         self.response.out.write(template.render(vars))
 
 class SchedulePage(webapp2.RequestHandler):
     def get(self):
         vars = {'page': 'home'}
-    	template = JINJA_ENVIRONMENT.get_template('schedule.html')
+    	template = JINJA_ENVIRONMENT.get_template('student/schedule.html')
         self.response.out.write(template.render(vars))
 
 class SafetyPage(webapp2.RequestHandler):
     def get(self):
         vars = {'page': 'home'}
-    	template = JINJA_ENVIRONMENT.get_template('safety.html')
+    	template = JINJA_ENVIRONMENT.get_template('student/safety.html')
         self.response.out.write(template.render(vars))
 
 class VisaPage(webapp2.RequestHandler):
     def get(self):
         vars = {'page': 'home'}
-        template = JINJA_ENVIRONMENT.get_template('visa.html')
+        template = JINJA_ENVIRONMENT.get_template('student/visa.html')
         self.response.out.write(template.render(vars))
 
 class AccommodationPage(webapp2.RequestHandler):
     def get(self):
         vars = {'page': 'home'}
-    	template = JINJA_ENVIRONMENT.get_template('accommodation.html')
+    	template = JINJA_ENVIRONMENT.get_template('student/accommodation.html')
         self.response.out.write(template.render(vars))
 
 class AboutPage(webapp2.RequestHandler):
     def get(self):
         vars = {'page': 'about'}
-        template = JINJA_ENVIRONMENT.get_template('about.html')
+        template = JINJA_ENVIRONMENT.get_template('student/about.html')
         self.response.out.write(template.render(vars))
 
 class ContactPage(webapp2.RequestHandler):
     def get(self):
         vars = {'page': 'contact'}
-        template = JINJA_ENVIRONMENT.get_template('contact.html')
+        template = JINJA_ENVIRONMENT.get_template('student/contact.html')
         self.response.out.write(template.render(vars))
 
 class TestimonialsPage(webapp2.RequestHandler):
     def get(self):
         vars = {'page': 'testimonial'}
-        template = JINJA_ENVIRONMENT.get_template('testimonials.html')
+        template = JINJA_ENVIRONMENT.get_template('student/testimonials.html')
         self.response.out.write(template.render(vars))
 
 class StudentDetailFormPage(webapp2.RequestHandler):
     def get(self):
         vars = {'page': 'studentdetailform'}
-        template = JINJA_ENVIRONMENT.get_template('studentdetailform.html')
+        template = JINJA_ENVIRONMENT.get_template('student/studentdetailform.html')
         self.response.out.write(template.render(vars))
 
 class CounselorHomePage(webapp2.RequestHandler):
